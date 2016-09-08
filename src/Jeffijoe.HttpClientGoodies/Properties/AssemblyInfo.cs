06d79e4d88b0ac918b7f7af4a245cfebcba56926
Jeff Hansen <jeffijoe@hotmail.com>
2016-09-08 21:12:41 +0200

    Added NuSpec file.

--- src/Jeffijoe.HttpClientGoodies/Properties/AssemblyInfo.cs
+++ src/Jeffijoe.HttpClientGoodies/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Jeffijoe.HttpClientGoodies")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Utilities for .NET HttpClient")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jeffijoe")]
 [assembly: AssemblyProduct("Jeffijoe.HttpClientGoodies")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Jeff Hansen 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.*")]
